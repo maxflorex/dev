d90629b7f9a24b0af56d85285065341f16b03b55
Max Flores <max@maxflores.dev>
2022-06-21 19:51:05 -0600

    bp

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+const Home: FC = () => {
+    return (
+        <div className="flex-center bg-teal-200 py-24">
+            <h1 className="text-center w-full text-6xl">This is my home</h1>
+            <button className="btn">Click me</button>
+        </div>
+    );
+};
+
+export default Home;
---
+++ windi.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'windicss/helpers'
+import formsPlugin from 'windicss/plugin/forms'
+
+export default defineConfig({
+    darkMode: 'class',
+    safelist: 'p-3 p-4 p-5',
+    theme: {
+        extend: {
+            colors: {
+                teal: {
+                    100: '#096',
+                },
+            },
+        },
+    },
+    plugins: [formsPlugin],
+    shortcuts: {
+        'btn': 'py-2 px-4 font-bold text-xl rounded-lg shadow bg-teal-500',
+        'flex-center': 'flex flex-col items-center justify-center w-full h-full gap-4 p-8'
+    }
+})
