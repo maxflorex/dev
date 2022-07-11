0b72b7ed81585380bbff8b2cd2252c3d5bc7e571
Max Flores <max@maxflores.dev>
2022-07-11 14:00:57 -0600

    Spline

--- src/components/01-home/Spline.tsx
+++ src/components/01-home/Spline.tsx
@@ -1,13 +1,20 @@
-import React, { Suspense } from 'react';
-import Loading from '../Loading';
-const Spline = React.lazy(() => import('@splinetool/react-spline'));
+import Spline from '@splinetool/react-spline';
+import { motion } from 'framer-motion';
 
 const Animation = () => {
     return (
         <div className="md:(w-1/2 block) h-full flex hidden">
-                <Suspense fallback={<Loading />}>
-                    <Spline scene="https://prod.spline.design/OF5o0oiDijGXsDow/scene.splinecode" className='md:(h-full w-full) h-auto w-screen -mb-60' />
-                </Suspense>
+            <motion.div
+                className="w-full h-full"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 1, duration: 1 }}
+            >
+                <Spline
+                    scene="https://prod.spline.design/OF5o0oiDijGXsDow/scene.splinecode"
+                    className="md:(h-full w-full) h-auto w-screen -mb-60"
+                />
+            </motion.div>
         </div>
     );
 };
