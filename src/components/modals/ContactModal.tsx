c7bd4aa312e949b44d53ccc0593210004b157773
Max Flores <max@maxflores.dev>
2022-07-13 22:56:57 -0600

    Contact Form

--- src/components/modals/ContactModal.tsx
+++ src/components/modals/ContactModal.tsx
@@ -36,71 +36,11 @@ const ContactModal = ({ setShowContact }: Props) => {
                 className="fixed top-8 right-8 text-2xl dismiss cursor-pointer hover:animate-spin"
                 onClick={handleClick}
             />
-            {/* <form className="w-full max-w-lg bg-white p-16 shadow">
-                <div className="flex flex-wrap -mx-3 mb-6">
-                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label className="block font-serif tracking-wide text-navy text-xs mb-2 text-xl">
-                            First Name
-                        </label>
-
-                        <input
-                            className="text-xs appearance-none block w-full bg-off/40 text-navy border border-none rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-off"
-                            id="grid-last-name"
-                            type="text"
-                            placeholder="John"
-                        />
-                    </div>
-                    <div className="w-full md:w-1/2 px-3">
-                        <label className="block font-serif tracking-wide text-navy text-xs mb-2 text-xl">
-                            Last Name
-                        </label>
-                        <input
-                            className="text-xs appearance-none block w-full bg-off/40 text-navy border border-none rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-off"
-                            id="grid-last-name"
-                            type="text"
-                            placeholder="Doe"
-                        />
-                    </div>
-                </div>
-                <div className="flex flex-wrap -mx-3 mb-6">
-                    <div className="w-full px-3">
-                        <label className="block font-serif tracking-wide text-navy text-xs mb-2 text-xl">
-                            E-mail
-                        </label>
-                        <input
-                            className="text-xs appearance-none block w-full bg-off/40 text-navy border border-none rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-off"
-                            id="email"
-                            type="email"
-                            placeholder="you@email.com"
-                        />
-                    </div>
-                </div>
-                <div className="flex flex-wrap -mx-3 mb-6">
-                    <div className="w-full px-3">
-                        <label className="block font-serif tracking-wide text-navy text-xs mb-2 text-xl">
-                            Message
-                        </label>
-                        <textarea
-                            className=" no-resize text-xs appearance-none block w-full bg-off/40 text-navy border border-none rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-off h-48 resize-none"
-                            id="message"
-                            placeholder="Place your message here..."
-                        ></textarea>
-                    </div>
-                </div>
-                <div className="md:flex md:items-center">
-                    <div className="md:w-1/3">
-                        <button className="btn" type="button">
-                            Send
-                        </button>
-                    </div>
-                    <div className="md:w-2/3"></div>
-                </div>
-            </form> */}
             <div className="bg-azul p-16 flex flex-col justify-center items-center gap-8 rounded-sm">
                 <img src={mail3d} alt="" className="w-full md:-my-32 -my-16" />
                 <h1 className="md:text-6xl text-4xl font-serif">Hit me up!</h1>
                 <p className="md:text-2xl text-xl text-white">
-                    <a href="mailto:someone@example.com">max@maxflores.dev</a>
+                    <a href="mailto:max@maxflores.dev">max@maxflores.dev</a>
                 </p>
             </div>
         </motion.div>
